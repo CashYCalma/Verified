d26eb28858f5a060c9bbd17af1b84fa92da725e9
CashYCalma <elmer.aguilar2620@gmail.com>
2026-02-15 20:17:52 -0600

    Update verify.js

--- verify.js
+++ verify.js
@@ -29,3 +29,14 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 
 });
+// DROP DINÁMICO PARA NFC
+const params = new URLSearchParams(window.location.search);
+const drop = params.get("drop");
+
+const dropElement = document.getElementById("dropNumber");
+
+if(drop && drop >= 1 && drop <= 50){
+  dropElement.textContent = "Drop 01 • " + drop + " of 50";
+} else {
+  dropElement.textContent = "Drop 01 • Limited Edition";
+}
